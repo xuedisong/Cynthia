d62ac3b6435ee10f7130668ef2ac94357b4f15cb
lixin <1583477527@qq.com>
2023-05-07 16:29:55 +0800

    智能指针其实是侵入业务代码的

--- Like.cpp
+++ Like.cpp
@@ -7,16 +7,22 @@
 using namespace std;
 
 
-void input(vector<int> *p1) {
+void input(shared_ptr<vector<int> > p1) {
     int i;
     cin >> i;
     p1->push_back(i);
 }
 
+//int main() {
+//    vector<int> *p = new(nothrow)vector<int>();
+//    input(p);
+//    cout << p->front() << ',' << p->back();
+//    delete p;
+//    p = nullptr;
+//}
+
 int main() {
-    vector<int> *p = new(nothrow)vector<int>();
+    auto p = make_shared<vector<int> >();
     input(p);
     cout << p->front() << ',' << p->back();
-    delete p;
-    p = nullptr;
 }
